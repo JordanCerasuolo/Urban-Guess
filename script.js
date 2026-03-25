bd0c3d21db7801a78b9583c351c8d67b7ecd18b2
sschrock04 <144834884+sschrock04@users.noreply.github.com>
2026-03-25 11:45:41 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const input = document.getElementById("guessInput");
+const button = document.getElementById("submitBtn");
+const container = document.getElementById('buttonContainer');
+let guesses = 0;
+
+button.addEventListener("click", function() {
+    const guess = input.value;
+    const answer = "San Antonio"
+    if(guess.toLowerCase() != answer.toLowerCase() && guesses == 0) {
+        alert("Not quite! Here's a hint: This city is the 7th largest by population in the United States.");
+    }
+    if(guess.toLowerCase() != answer.toLowerCase() && guesses == 1) {
+        alert("That's not quite right. Here's another hint: Remember the Alamo!");
+    }
+    if(guess.toLowerCase() != answer.toLowerCase() && guesses == 2) {
+        alert("Sorry that's incorrect. \nScore: 0");
+    }
+    if(guess.toLowerCase() == answer.toLowerCase()) {
+        const score = 5 - (guesses * 2);
+        alert("Correct! \nScore: " + score);
+        const learnButton = document.createElement('button');
+        learnButton.textContent = 'Learn More!';
+        learnButton.className = 'button';
+
+        learnButton.addEventListener('click', function() {
+            window.location.href = "https://en.wikipedia.org/wiki/San_Antonio";
+        });
+
+        container.appendChild(learnButton);
+
+        const gamesButton = document.createElement('button');
+        gamesButton.textContent = 'Games Page';
+        gamesButton.className = 'button';
+
+        gamesButton.addEventListener('click', function() {
+            window.location.href = "games.html";
+        });
+
+        container.appendChild(gamesButton);
+    }
+    guesses++;
+    document.getElementById('guessInput').value = '';
+});
+
+
+
